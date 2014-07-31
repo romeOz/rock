4740677814d1b2e405e76b7097a626f2f08c6460
romeo <serggalka@gmail.com>
2014-07-31 10:58:26 +0400

    Added support RecursiveTreeIterator for HHVM.

--- tests/data/travis/apc-setup.sh
+++ tests/data/travis/apc-setup.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
 if (php --version | grep -i HHVM > /dev/null); then
-  echo "skipping APC on HHVM"
+    echo "skipping APC on HHVM"
 else
+    # this is helpful to compile extension
+    sudo apt-get install autoconf
+    sudo apt-get install phpize
+    
+    # install this version
+    APCU=4.0.6
 
-# this is helpful to compile extension
-sudo apt-get install autoconf
+    # compile manually, because `pecl install apcu-beta` keep asking questions
+    wget http://pecl.php.net/get/apcu-$APCU.tgz
+    tar zxvf apcu-$APCU.tgz
+    cd "apcu-${APCU}"
+    phpize && ./configure && make install && echo "Installed ext/apcu-${APCU}"
 
-# install this version
-APCU=4.0.6
-
-# compile manually, because `pecl install apcu-beta` keep asking questions
-wget http://pecl.php.net/get/apcu-$APCU.tgz
-tar zxvf apcu-$APCU.tgz
-cd "apcu-${APCU}"
-phpize && ./configure && make install && echo "Installed ext/apcu-${APCU}"
-
-echo "extension = apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
+    echo "extension = apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 fi
