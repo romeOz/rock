392d3f08fe635d582ec487304fd17eb861b294b6
romeo <serggalka@gmail.com>
2014-08-24 07:55:06 +0400

    Skippied MQ on HHVM-nightly.

--- tests/data/travis/mq-setup.sh
+++ tests/data/travis/mq-setup.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-if (php --version | grep -i HHVM > /dev/null); then
-    echo "skipping APC on HHVM"
+if (php --version | grep -i -e "^HHVM" > /dev/null); then
+    echo "skipping mq on HHVM"
 else
     sudo add-apt-repository -y ppa:chris-lea/zeromq
-    #sudo add-apt-repository -y ppa:ondrej/php5
+    sudo add-apt-repository -y ppa:ondrej/php5
     sudo apt-get update
 
     # Install ZeroMQ
     sudo apt-get install libzmq3 libpgm-5.1-0 php5-zmq
 
     # Install Gearman
-    sudo apt-get install gearman-job-server libev-libevent-dev uuid-dev php-pear
-    sudo pecl install gearman
-    echo "extension=gearman.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    sudo apt-get install gearman-job-server php5-gearman
 
     # Install RabbitMQ
     #sudo apt-get install rabbitmq-server
