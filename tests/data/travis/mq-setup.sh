3c6e3789295cfda50abf31aff2ce4fa58f2c7f00
romeo <serggalka@gmail.com>
2014-08-24 16:29:50 +0400

    Changed mq-setup.sh.

--- tests/data/travis/mq-setup.sh
+++ tests/data/travis/mq-setup.sh
@@ -11,10 +11,13 @@ else
     sudo apt-get install libzmq3 libpgm-5.1-0 php5-zmq
 
     # Install Gearman
-    sudo apt-get install gearman-job-server php5-gearman
+    sudo apt-get install gearman-job-server
+    sudo apt-get install php5-gearman
 
     php tests/data/mq/zero/simple_server.php &
     php tests/data/mq/zero/pub_server.php &
+
+    php tests/data/mq/gearman/simple_server.php &
     # Install RabbitMQ
     #sudo apt-get install rabbitmq-server
 fi
