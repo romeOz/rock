57f6b16a680649608a52b109b048dc9edb950301
romeo <serggalka@gmail.com>
2014-08-24 17:12:18 +0400

    Fixed mq-setup.mq: install gearman.

--- tests/data/travis/mq-setup.sh
+++ tests/data/travis/mq-setup.sh
@@ -12,8 +12,20 @@ else
 
     # Install Gearman
     sudo apt-get install gearman-job-server
-    sudo apt-get install php5-gearman
-    echo "extension = gearman.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    #sudo apt-get install php5-gearman
+    #echo "extension = gearman.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    sudo apt-get install libboost-all-dev
+    sudo apt-get install gperf
+    sudo apt-get install libuuid1
+    sudo apt-get install uuid-dev
+    wget https://launchpad.net/gearmand/1.2/1.1.11/+download/gearmand-1.1.11.tar.gz
+    tar xf gearmand-1.1.11.tar.gz
+    cd gearmand-1.1.11
+    ./configure
+    make
+    sudo make install
+    cd -
+    yes | pecl install gearman
 
     php tests/data/mq/zero/simple_server.php &
     php tests/data/mq/zero/pub_server.php &
