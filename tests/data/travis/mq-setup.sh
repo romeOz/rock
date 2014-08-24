cbbdbc9c5132d8ae1e821d90155ead84cd1f3c06
romeo <serggalka@gmail.com>
2014-08-24 07:12:53 +0400

    Changed mq-setup.sh.

--- tests/data/travis/mq-setup.sh
+++ tests/data/travis/mq-setup.sh
@@ -4,14 +4,14 @@ if (php --version | grep -i HHVM > /dev/null); then
     echo "skipping APC on HHVM"
 else
     sudo add-apt-repository -y ppa:chris-lea/zeromq
-    sudo add-apt-repository -y ppa:ondrej/php5
+    #sudo add-apt-repository -y ppa:ondrej/php5
     sudo apt-get update
 
     # Install ZeroMQ
     sudo apt-get install libzmq3 libpgm-5.1-0 php5-zmq
 
     # Install Gearman
-    sudo apt-get install gearman-job-server php5-gearman
+    sudo apt-get install gearman-job-server php-gearman
 
     # Install RabbitMQ
     #sudo apt-get install rabbitmq-server
