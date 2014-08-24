bcb2840e004c7b6c9ec304e1c3a342cd6d9390fa
romeo <serggalka@gmail.com>
2014-08-24 06:44:23 +0400

    Added mq-setup.sh + Refactoring.

---
+++ tests/data/travis/mq-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if (php --version | grep -i HHVM > /dev/null); then
+    echo "skipping APC on HHVM"
+else
+    # Install ZeroMQ
+    sudo add-apt-repository ppa:chris-lea/zeromq
+    sudo apt-get update
+    sudo apt-get install libzmq3 php5-zmq
+
+    # Install Gearman
+    sudo apt-get install gearman-job-server php5-gearman
+
+    # Install RabbitMQ
+    #sudo apt-get install rabbitmq-server
+fi
