6901e187451a9890030f55f2225c80ec523892a1
romeo <serggalka@gmail.com>
2014-09-02 12:08:58 +0400

    [skip CI] Fixed message of Validation.

--- framework/core/validation/exceptions/ru/MinException.php
+++ framework/core/validation/exceptions/ru/MinException.php
@@ -8,11 +8,11 @@ class MinException extends \rock\validation\exceptions\MinException
     public static $defaultTemplates = array(
         self::MODE_DEFAULT => array(
             self::STANDARD => '{{name}} должно быть больше {{minValue}}',
-            self::INCLUSIVE => '{{name}} должно быть больше или равно{{minValue}}',
+            self::INCLUSIVE => '{{name}} должно быть больше или равно {{minValue}}',
         ),
         self::MODE_NEGATIVE => array(
             self::STANDARD => '{{name}} не должно быть больше {{minValue}}',
-            self::INCLUSIVE => '{{name}} не должно быть больше или равно{{minValue}}',
+            self::INCLUSIVE => '{{name}} не должно быть больше или равно {{minValue}}',
         )
     );
 } 
