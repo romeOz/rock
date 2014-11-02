91e535de43ec9078bf0f35702d7455a8a4e56b92
romeo <serggalka@gmail.com>
2014-11-02 14:41:54 +0300

    Added imagick.so in .travis.

--- tests/data/travis/image-setup.sh
+++ tests/data/travis/image-setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-#if (php --version | grep -i HipHop > /dev/null); then
-#    echo "Skipping APC on HHVM"
-#    exit 0
-#fi
+if (php --version | grep -i HipHop > /dev/null); then
+    echo "Skipping APC on HHVM"
+    exit 0
+fi
 
 sudo apt-get install php5-imagick
+
+echo "extension = imagick.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
