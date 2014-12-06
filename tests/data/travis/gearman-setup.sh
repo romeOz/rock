435c4249d482e3d089bf3c40ef1746afd992a2b5
romeOz <serggalka@gmail.com>
2014-12-06 21:05:56 +0300

    Revert: skip Gearman tests.

--- tests/data/travis/gearman-setup.sh
+++ tests/data/travis/gearman-setup.sh
@@ -4,24 +4,12 @@ if (php --version | grep -i HipHop > /dev/null); then
     echo "Skipping Gearman on HHVM"
     exit 0
 fi
-sudo add-apt-repository -y ppa:ondrej/php5
+sudo add-apt-repository -y ppa:ondrej/php5-5.6
 sudo apt-get update
 
 # Install Gearman
-sudo apt-get install -y libboost-all-dev
-sudo apt-get install -y gperf libevent-dev uuid-dev libcloog-ppl-dev
 sudo apt-get install -y libgearman-dev
-wget https://launchpad.net/gearmand/1.2/1.1.12/+download/gearmand-1.1.12.tar.gz
-tar xf gearmand-1.1.12.tar.gz
-cd gearmand-1.1.12
-./configure
-make
-sudo make install
-cd -
-
-# Install pecl gearman
-yes | pecl install gearman
-
+sudo apt-get install -y php5-gearman
 
 # Install gearman-server
 sudo apt-get install gearman-job-server
