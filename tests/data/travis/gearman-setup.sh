37b5c53455984d4451b2684c96a1a2e53fa45c8b
romeOz <serggalka@gmail.com>
2014-12-06 08:39:11 +0300

    Fixed gearman-setup.sh

--- tests/data/travis/gearman-setup.sh
+++ tests/data/travis/gearman-setup.sh
@@ -4,25 +4,12 @@ if (php --version | grep -i HipHop > /dev/null); then
     echo "Skipping Gearman on HHVM"
     exit 0
 fi
+sudo add-apt-repository -y ppa:ondrej/php5-5.6
+sudo apt-get update
 
 # Install Gearman
-sudo apt-get install gearman-job-server
-#sudo apt-get install php5-gearman
-#echo "extension = gearman.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-sudo apt-get install libboost-all-dev
-sudo apt-get install gperf
-sudo apt-get install libuuid1
-#sudo apt-get install libev-libevent-dev
-sudo apt-get install uuid-dev
-sudo apt-get install libgearman-dev
-wget https://launchpad.net/gearmand/1.2/1.1.11/+download/gearmand-1.1.11.tar.gz
-tar xf gearmand-1.1.11.tar.gz
-cd gearmand-1.1.11
-./configure
-make
-sudo make install
-cd -
-yes | pecl install gearman
+sudo apt-get install -y gearman-job-server
+sudo apt-get install -y php5-gearman
 
 # Run servers (workers)
 php tests/data/mq/gearman/simple_server.php &
