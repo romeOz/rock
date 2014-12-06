399e56b3ff4644182990098babebe3f6e4fd96e3
romeOz <serggalka@gmail.com>
2014-12-06 08:57:09 +0300

    Fixed gearman-setup.sh

--- tests/data/travis/gearman-setup.sh
+++ tests/data/travis/gearman-setup.sh
@@ -10,6 +10,7 @@ sudo apt-get update
 # Install Gearman
 sudo apt-get install -y gearman-job-server
 sudo apt-get install -y php5-gearman
+echo "extension = gearman.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Run servers (workers)
 php tests/data/mq/gearman/simple_server.php &
