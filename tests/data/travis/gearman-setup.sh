134d959ba19488cde8baa42969a9e12a1fea5cf6
romeOz <serggalka@gmail.com>
2014-12-06 21:32:38 +0300

    Fixed gearman-setup.sh

--- tests/data/travis/gearman-setup.sh
+++ tests/data/travis/gearman-setup.sh
@@ -4,11 +4,11 @@ if (php --version | grep -i HipHop > /dev/null); then
     echo "Skipping Gearman on HHVM"
     exit 0
 fi
-sudo add-apt-repository -y ppa:ondrej/php5-5.6
+sudo add-apt-repository -y ppa:ondrej/php5
 sudo apt-get update
 
 # Install Gearman
-sudo apt-get install -y libgearman6
+sudo apt-get install -y libgearman-dev
 sudo apt-get install -y php5-gearman
 
 # Install gearman-server
