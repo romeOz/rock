4996814b058c168df26bca81310f15cf7d8b2989
romeOz <serggalka@gmail.com>
2014-12-10 14:00:00 +0300

    Enable Gearman.

--- tests/data/travis/gearman-setup.sh
+++ tests/data/travis/gearman-setup.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
-
 if (php --version | grep -i HipHop > /dev/null); then
-    echo "Skipping Gearman on HHVM"
-    exit 0
+echo "Skipping Gearman on HHVM"
+exit 0
 fi
-#sudo add-apt-repository -y ppa:ondrej/php5
-#sudo apt-get update
 
+sudo add-apt-repository -y ppa:ondrej/php5
+sudo apt-get update
 # Install Gearman
-sudo apt-get install -y libgearman-dev php5-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y gperf libevent-dev uuid-dev libcloog-ppl-dev
+sudo apt-get install -y libgearman-dev
+wget https://launchpad.net/gearmand/1.2/1.1.12/+download/gearmand-1.1.12.tar.gz
+tar xf gearmand-1.1.12.tar.gz
+cd gearmand-1.1.12
+./configure
+make
+sudo make install
 cd -
-
 # Install pecl gearman
-yes | pecl install channel://pecl.php.net/gearman-1.1.2
-
+yes | pecl install gearman
 # Install gearman-server
 sudo apt-get install gearman-job-server
-
 cd -
 # Run servers (workers)
 php tests/data/mq/gearman/simple_server.php &
-
