dff608903c4f2dae33dab5fc7d890cf9839b6797
romeOz <serggalka@gmail.com>
2014-12-11 22:19:30 +0300

    Fixed gearman-setup.sh.

--- tests/data/travis/gearman-setup.sh
+++ tests/data/travis/gearman-setup.sh
@@ -7,31 +7,24 @@ fi
 sudo add-apt-repository -y ppa:ondrej/php5
 sudo apt-get update
 
-# Install gearman-server
-sudo apt-get install -y gearman-job-server
-
 # Install Gearman
-sudo apt-get install -y libboost-all-dev
-sudo apt-get install -y gperf
-sudo apt-get install -y libuuid1
+sudo apt-get install gearman-job-server
+#sudo apt-get install php5-gearman
+#echo "extension = gearman.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+sudo apt-get install libboost-all-dev
+sudo apt-get install gperf
+sudo apt-get install libuuid1
 #sudo apt-get install libev-libevent-dev
-sudo apt-get install -y uuid-dev
-sudo apt-get install -y libgearman-dev
-
-cd ~
-
+sudo apt-get install uuid-dev
+sudo apt-get install libgearman-dev
 wget https://launchpad.net/gearmand/1.2/1.1.11/+download/gearmand-1.1.11.tar.gz
 tar xf gearmand-1.1.11.tar.gz
 cd gearmand-1.1.11
 ./configure
 make
 sudo make install
-
-sudo ldconfig
 cd ~
-# Install pecl gearman
 yes | pecl install gearman
-#echo "extension=gearman.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 cd ~
 # Run servers (workers)
