dd15f33ff594778dce82b0cc9aab0c95eb428c70
romeOz <serggalka@gmail.com>
2014-12-12 09:23:50 +0300

    Fixed GearmanQueueTest.php.

--- tests/extensions/mq/GearmanQueueTest.php
+++ tests/extensions/mq/GearmanQueueTest.php
@@ -16,25 +16,25 @@ class GearmanQueueTest extends \PHPUnit_Framework_TestCase
     {
         //non blocking
         $gearman = new GearmanQueue();
-//        $gearman->blocking = false;
-//        $gearman->sendBackground('background');
-//
-//        //blocking
-//        $gearman->blocking = true;
-//        $gearman->sendBackground('background');
+        $gearman->blocking = false;
+        $gearman->sendBackground('background');
 
-//        //non blocking
-//        $gearman->blocking = false;
-//        $this->assertSame('I am server: test', $gearman->send('test'));
-//
+        //blocking
+        $gearman->blocking = true;
+        $gearman->sendBackground('background');
+
+        //non blocking
+        $gearman->blocking = false;
+        $this->assertSame('I am server: test', $gearman->send('test'));
+
         //blocking
         $gearman->blocking = true;
         $this->assertSame('I am server: test', $gearman->send('test'));
-//
-//        // unknown queueName
-//        $gearman->id = 'demo';
-//        $gearman->blocking = false;
-//        $this->assertNull($gearman->send('test', 2));
+
+        // unknown queueName
+        $gearman->id = 'demo';
+        $gearman->blocking = false;
+        $this->assertNull($gearman->send('test', 2));
     }
 }
  
