36910eeb3c820208fcbc016b7f4a7e1427601432
romeOz <serggalka@gmail.com>
2014-12-12 05:55:53 +0300

    Fixed gearman-setup.sh.

--- tests/data/travis/gearman-setup.sh
+++ tests/data/travis/gearman-setup.sh
@@ -6,28 +6,25 @@ fi
 
 sudo add-apt-repository -y ppa:ondrej/php5
 sudo apt-get update
-
 # Install Gearman
-sudo apt-get install gearman-job-server
-#sudo apt-get install php5-gearman
-#echo "extension = gearman.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-sudo apt-get install libboost-all-dev
-sudo apt-get install gperf
-sudo apt-get install libuuid1
-#sudo apt-get install libev-libevent-dev
-sudo apt-get install uuid-dev
-sudo apt-get install libgearman-dev
-wget https://launchpad.net/gearmand/1.2/1.1.11/+download/gearmand-1.1.11.tar.gz
-tar xf gearmand-1.1.11.tar.gz
-cd gearmand-1.1.11
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y gperf uuid-dev
+#sudo apt-get install -y gperf libevent-dev uuid-dev libcloog-ppl-dev
+sudo apt-get install -y libgearman-dev
+wget https://launchpad.net/gearmand/1.2/1.1.12/+download/gearmand-1.1.12.tar.gz
+tar xf gearmand-1.1.12.tar.gz
+cd gearmand-1.1.12
 ./configure
 make
 sudo make install
-cd ~
-yes | pecl install gearman
 
 sudo ldconfig
-
+cd ~
+# Install pecl gearman
+yes | pecl install gearman
+#echo "extension=gearman.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+# Install gearman-server
+sudo apt-get install -y gearman-job-server
 cd ~
 # Run servers (workers)
 php tests/data/mq/gearman/simple_server.php &
