f13e48375f357f6bf9ee8b4ea240215d39b0309d
romeOz <serggalka@gmail.com>
2015-01-13 13:13:34 +0300

    Added `testSendFile` for `Response`.

--- tests/core/ResponseTest.php
+++ tests/core/ResponseTest.php
@@ -3,7 +3,9 @@ namespace rockunit\core;
 
 use rock\base\Controller;
 use rock\helpers\Json;
+use rock\helpers\StringHelper;
 use rock\response\Response;
+use rock\response\ResponseException;
 use rock\Rock;
 use rock\route\Route;
 
@@ -47,7 +49,9 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
 
     protected function tearDown()
     {
-        Rock::$app->response->isSent = false;
+        $response = Rock::$app->response;
+        $response->format = Response::FORMAT_HTML;
+        $response->isSent = false;
     }
 
 
@@ -70,7 +74,6 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-
     public function testGetXml()
     {
         (new Route())->get(
@@ -84,8 +87,63 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
             "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<response><foo>text</foo><item>bar</item></response>");
     }
 
-    public static function tearDownAfterClass()
+
+    public function rightRanges()
     {
-        Rock::$app->response->format = Response::FORMAT_HTML;
+        // http://www.w3.org/Protocols/rfc2616/rfc2616.txt
+        return [
+            ['0-5', '0-5', 6, '12ёж'],
+            ['2-', '2-66', 65, 'ёжик3456798áèabcdefghijklmnopqrstuvwxyz!"§$%&/(ёжик)=?'],
+            ['-12', '55-66', 12, '(ёжик)=?'],
+        ];
+    }
+
+    /**
+     * @dataProvider rightRanges
+     */
+    public function testSendFileRanges($rangeHeader, $expectedHeader, $length, $expectedContent)
+    {
+        /** @var Response $response */
+        $response = new Response();
+        $dataFile = Rock::getAlias('@rockunit/data/response/data.txt');
+        $fullContent = file_get_contents($dataFile);
+        $_SERVER['HTTP_RANGE'] = 'bytes=' . $rangeHeader;
+        ob_start();
+        $response->sendFile($dataFile)->send();
+        $content = ob_get_clean();
+
+        $this->assertEquals($expectedContent, $content);
+        $this->assertEquals(206, $response->statusCode);
+        $headers = $response->headers;
+        $this->assertEquals("bytes", $headers->get('Accept-Ranges'));
+        $this->assertEquals("bytes " . $expectedHeader . '/' . StringHelper::byteLength($fullContent), $headers->get('Content-Range'));
+        $this->assertEquals('text/plain', $headers->get('Content-Type'));
+        $this->assertEquals("$length", $headers->get('Content-Length'));
+    }
+
+    public function wrongRanges()
+    {
+        // http://www.w3.org/Protocols/rfc2616/rfc2616.txt
+        return [
+            ['1-2,3-5,6-10'],	// multiple range request not supported
+            ['5-1'],			// last-byte-pos value is less than its first-byte-pos value
+            ['-100000'],		// last-byte-pos bigger then content length
+            ['10000-'],			// first-byte-pos bigger then content length
+        ];
+    }
+
+    /**
+     * @dataProvider wrongRanges
+     */
+    public function testSendFileWrongRanges($rangeHeader)
+    {
+        $this->setExpectedException(ResponseException::className());
+
+        /** @var Response $response */
+        $response = new Response();
+
+        $dataFile = Rock::getAlias('@rockunit/data/response/data.txt');
+        $_SERVER['HTTP_RANGE'] = 'bytes=' . $rangeHeader;
+        $response->sendFile($dataFile);
     }
 }
