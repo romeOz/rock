30e1f0cd5257f9265482614f81f20f92c776d0d6
romeOz <serggalka@gmail.com>
2015-01-18 00:07:19 +0300

    Light refactoring: Unique extends `\rock\validate\locale\en\Unique`.

--- framework/core/validate/locale/ru/Unique.php
+++ framework/core/validate/locale/ru/Unique.php
@@ -3,32 +3,7 @@
 namespace rock\validate\locale\ru;
 
 
-use rock\validate\locale\Locale;
-
-class Unique extends Locale
+class Unique extends \rock\validate\locale\en\Unique
 {
-    public function defaultTemplates()
-    {
-        return [
-            self::MODE_DEFAULT => [
-                self::STANDARD => $this->i18n->translate('unique'),
-            ],
-            self::MODE_NEGATIVE => [
-                self::STANDARD => $this->i18n->translate('notUnique'),
-            ]
-        ];
-    }
 
-    public function defaultPlaceholders($value = null)
-    {
-        if (isset($value)) {
-            if (is_array($value)) {
-                $value = implode(',', $value);
-            }
-            $value = "{$value}";
-        }
-        return [
-            'value' =>  $value
-        ];
-    }
 }
