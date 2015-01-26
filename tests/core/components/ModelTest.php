7028d5792751d2e0510bd4d4a27ed34cb320c8e1
romeOz <serggalka@gmail.com>
2015-01-26 11:12:47 +0300

    Refactoring tests for Model.

--- tests/core/components/ModelTest.php
+++ tests/core/components/ModelTest.php
@@ -1,14 +1,70 @@
 <?php
-namespace rockunit\core\base;
+namespace rockunit\core\components;
 
+use rock\base\ObjectTrait;
 use rock\components\Model;
 use rock\i18n\i18n;
 
 /**
  * @group base
  */
-class ModelRulesTest extends \PHPUnit_Framework_TestCase
+class ModelTest extends \PHPUnit_Framework_TestCase
 {
+    public function testToArray()
+    {
+        $object = new Post1;
+        $this->assertEquals(get_object_vars($object), Model::convert($object));
+        $object = new Post2;
+        $this->assertEquals(get_object_vars($object), Model::convert($object));
+        $object1 = new Post1;
+        $object2 = new Post2;
+        $this->assertEquals(
+            [
+                get_object_vars($object1),
+                get_object_vars($object2),
+            ],
+            Model::convert(
+                [
+                    $object1,
+                    $object2,
+                ]
+            )
+        );
+        $object = new Post2;
+
+        $expected = [
+            'id' => 123,
+            'secret' => 's',
+            '_content' => 'test',
+            'length' => 4,
+        ];
+        $actual = Model::convert(
+            $object,
+            [
+                $object->className() => [
+                    'id', 'secret',
+                    '_content' => 'content',
+                    'length' => function ($post) {
+                        return strlen($post->content);
+                    }
+                ]
+            ]
+        );
+        $this->assertEquals($expected, $actual);
+        $object = new Post3();
+        $this->assertEquals(get_object_vars($object), Model::convert($object, [], false));
+
+        $expected = [
+            'id' => 33,
+            'subObject' => [
+                'id' => 123,
+                'content' => 'test',
+            ],
+        ];
+        $this->assertEquals($expected, Model::convert($object));
+    }
+
+
     public function testFilter()
     {
         $model = new FooModal();
@@ -224,7 +280,7 @@ class ModelRulesTest extends \PHPUnit_Framework_TestCase
         $expected = [
             'username' =>
                 [
-                   'foo must have a length between 6 and 20',
+                    'foo must have a length between 6 and 20',
                 ],
         ];
         $this->assertSame($expected, $model->getErrors());
@@ -395,5 +451,41 @@ class FooModal extends Model
             return false;
         }
         return true;
+    }
+}
+
+class Post1
+{
+    public $id = 23;
+    public $title = 'tt';
+}
+
+class Post2
+{
+    use ObjectTrait;
+
+    public $id = 123;
+    public $content = 'test';
+    private $secret = 's';
+
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+}
+
+class Post3
+{
+    use ObjectTrait {
+        ObjectTrait::__construct as parentConstruct;
+    }
+
+    public $id = 33;
+    public $subObject;
+
+    public function __construct(array $configs = [])
+    {
+        $this->parentConstruct($configs);
+        $this->subObject = new Post2();
     }
 }
