52f812e455061ffc9de88b517f6ee7fac5b6eeb7
romeOz <serggalka@gmail.com>
2015-03-12 23:26:33 +0300

    Removed polyfill: `RecursiveTreeIterator`.

--- src/polyfills.php
+++ src/polyfills.php
@@ -4,11 +4,3 @@ if (!function_exists('boolval')) {
         return (bool)$val;
     }
 }
-
-if (!class_exists('\RecursiveTreeIterator')) {
-    include __DIR__ .'/polyfills/RecursiveTreeIterator.php';
-}
-
-//if (!class_exists('\ZMQ')) {
-//    include __DIR__ .'/polyfills/ZMQ.php';
-//}
