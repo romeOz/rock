d95b7e33ea9c7e7770d79ee07f6a5ab0468b8883
romeOz <serggalka@gmail.com>
2015-09-03 04:49:38 +0300

    Simple adjustments to CSP filter. Does not conflict IE and Firefox <= 22.

--- src/filters/CSP.php
+++ src/filters/CSP.php
@@ -38,25 +38,42 @@ class CSP extends ActionFilter
     public function send()
     {
         $policy = [];
+        if (!isset($this->policy['default-src'])) {
+            $this->policy['default-src'] = "'self'";
+        }
         foreach ($this->policy as $name => $value) {
             if (is_string($value)) {
                 $value = str_replace('\'', '', $value);
-                $value = preg_replace('/\b(self|none|unsafe-eval|unsafe-inline)\b/', "'$0'", $value);
+                $value = preg_replace(['/\b(self|none|unsafe-eval|unsafe-inline)\b/', '/\s+/'], ["'$0'", ' '], $value);
                 $policy[] = $name . ' ' . rtrim($value, ';') . ';';
                 continue;
             }
             $value = implode(' ', $value);
             $value = str_replace('\'', '', $value);
-            $value = preg_replace('/\b(self|none|unsafe-eval|unsafe-inline)\b/', "'$0'", $value);
+            $value = preg_replace(['/\b(self|none|unsafe-eval|unsafe-inline)\b/', '/\s+/'], ["'$0'", ' '], $value);
             $policy[] = "{$name} {$value};";
         }
 
         if ($policy) {
             $policy = implode(' ', $policy);
-            // @link http://caniuse.com/#feat=contentsecuritypolicy
-            $this->response->getHeaders()
-                ->set('Content-Security-Policy', $policy)
-                ->set('X-Content-Security-Policy', $policy); // for IE10 or great
+
+            /* @link http://caniuse.com/#feat=contentsecuritypolicy
+             * Does not conflict IE and Firefox <= 22 @link http://habrahabr.ru/company/yandex/blog/206508/ and @link https://events.yandex.ru/lib/talks/2587/
+             */
+            if ($this->isIE()) {
+                $this->response->getHeaders()->set('X-Content-Security-Policy', $policy); // for IE10 or great (does not Edge)
+                return;
+            }
+            $this->response->getHeaders()->set('Content-Security-Policy', $policy);
         }
+    }
+
+    /**
+     * Check Internet Explorer (does not Edge).
+     * @return bool
+     */
+    protected function isIE()
+    {
+        return isset($_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false);
     }
 }
